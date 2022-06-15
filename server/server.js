d4b03c17439b579dc30dccaed4f4cd24825e90a1
Raul <jimenezraul1981@gmail.com>
2022-06-14 21:13:51 -0400

    initial commit

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+const PORT = process.env.PORT || 3001;
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/", async (req, res) => {
+    
+  try {
+    const response = await axios(process.env.STEPZEN_ENDPOINT, {
+      method: "POST",
+      headers: {
+        Authorization: `Apikey ${process.env.STEPZEN_AUTH}`,
+      },
+      data: req.body,
+    });
+    return res.status(200).json(response.data);
+  } catch (error) {
+    console.log("error", error.response);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`App is running on *${PORT}`);
+});
