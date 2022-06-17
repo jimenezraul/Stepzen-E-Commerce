b09f04df742419b4bf65ec9d995b788f4de4fbbf
Raul <jimenezraul1981@gmail.com>
2022-06-17 10:32:39 -0400

    added add and remove from cart and wishlist

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
@@ -9,6 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, "public")));
 
 app.post("/", async (req, res) => {
   try {
